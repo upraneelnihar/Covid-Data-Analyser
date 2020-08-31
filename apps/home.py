b3113d36503c6fbfee8873c18b4d482c825492bd
praneel <u.praneel.nihar@gmail.com>
2020-08-31 20:47:25 +0530

    adds home page.

---
+++ apps/home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def app():
+    st.markdown('# Covid-19 India Data Analyzer')
+
+    st.markdown('This dashboard analyses the Covid-19 situation in India.')
+
+    st.markdown('### Disease Details')
+    st.markdown('Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.')
+    st.markdown('Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.')
+
+    st.markdown('### How it spreads')
+    st.markdown('The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales. ')
+    st.markdown('These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.')
+    st.markdown('You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19,or by touching a contaminated surface and then your eyes, nose or mouth.')
+
+    st.markdown('### Safety Measures')
+    st.markdown('By following good respiratory hygiene, you protect the people around you from viruses such as cold, flu and COVID-19.')
+
+    st.markdown('Stay home and self-isolate even with minor symptoms such as cough, headache, mild fever, until you recover.')
+    st.markdown('Have someone bring you supplies. If you need to leave your house, wear a mask to avoid infecting others. ')
+    st.markdown('Why? Avoiding contact with others will protect them from possible COVID-19 and other viruses.')
+
+    st.markdown('If you have a fever, cough and difficulty breathing, seek medical attention, but call by telephone in advance if possible and follow the directions of your local health authority.')
+    st.markdown('Why? National and local authorities will have the most up to date information on the situation in your area.')
