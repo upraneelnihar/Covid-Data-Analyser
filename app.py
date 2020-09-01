d2bcd6a8e46281227cd59f466283314d254421e5
praneel <u.praneel.nihar@gmail.com>
2020-09-01 23:05:13 +0530

    adds forecast tab in main app.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from apps import home,eda
+from apps import home,eda,forecast
 from multiapp import MultiApp
 
 app = MultiApp()
 
 # Add all your application here
-app.add_app('Home', home.app)
-app.add_app('Home', eda.app)
+app.add_app('Intro', home.app)
+app.add_app('Explore Data', eda.app)
+app.add_app('Forecast', forecast.app)
 
 # The main app
 app.run()
