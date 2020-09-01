eb14b00022f02129eecf8639d2dcb1fb8d07bf6f
praneel <u.praneel.nihar@gmail.com>
2020-09-01 10:09:42 +0530

    adds EDA page to main app.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from apps import home
+from apps import home,eda
 from multiapp import MultiApp
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app('Home', home.app)
+app.add_app('Home', eda.app)
 
 # The main app
 app.run()
